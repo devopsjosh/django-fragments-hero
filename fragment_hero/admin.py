a8c5a36ff1c5fa1e4cbca75f62f83d28b5d2e3d3
Josh Tesch <jdtesch77@gmail.com>
2021-08-27 22:49:02 -0500

    Added cta to inlines

--- fragment_hero/admin.py
+++ fragment_hero/admin.py
@@ -2,19 +2,18 @@ from django.contrib import admin
 from fragment_hero.models import Hero, HeroCallToAction
 
 
+class HeroCallToActionInline(admin.StackedInline):
+    model = HeroCallToAction
+    extra = 1
+
+
 class HeroAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = [
         '__str__', 'name', 'start', 'end', 'is_active'
     ]
     list_filter = ('start', 'end',)
-
-
-class HeroCallToActionAdmin(admin.ModelAdmin):
-    search_fields = ['title']
-    list_display = ['title', 'hero']
-    list_filter = ('hero',)
+    inlines = [HeroCallToActionInline, ]
 
 
 admin.site.register(Hero, HeroAdmin)
-admin.site.register(HeroCallToAction, HeroCallToActionAdmin)
